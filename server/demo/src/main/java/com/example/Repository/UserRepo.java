c1d078a4f07d0d12038b586d71f755a25a470a9a
atsu <kinnjiroa@gmail.com>
2024-11-18 09:07:24 +0900

    r

---
+++ server/demo/src/main/java/com/example/Repository/UserRepo.java
@@ -0,0 +1,15 @@
+package com.example.Repository;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public class UserRepo {
+	
+	public void insertData() {
+	
+	}
+
+    
+}
